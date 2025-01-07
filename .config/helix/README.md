8a818ddaa3f84c15f2fa89c7154e67bbfe73e6aa
IQ <ian@ianq.ai>
2025-01-06 21:23:01 -0800

    right direction

---
+++ .config/helix/README.md
@@ -0,0 +1,8 @@
+# Setting Up
+
+
+After you've set everything up as per the top-level description, run
+
+`sudo ln -s $HOME/.config/helix/basedpyright-ls-helix.sh /usr/local/bin/basedpyright-ls-helix`
+
+which will put the `basedpyright-ls-helix` in the appropriate location
---
+++ .config/helix/basedpyright-ls-helix.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+if [[ $VIRTUAL_ENV ]]; then
+  python_path="${VIRTUAL_ENV}/bin/python"
+elif [[ $CONDA_PREFIX ]] ; then
+  python_path="${CONDA_PREFIX}/bin/python"
+else
+  python_path="python"
+fi
+
+export PYTHONPATH=python_path
+basedpyright-langserver --stdio
